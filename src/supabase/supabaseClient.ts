bbb30765f70ff6a2f0e36f32db5037c20fb38718
IgnRamosr <ign.ramosr@duocuc.cl>
2025-06-08 20:14:58 -0400

    Cambios en credenciales

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://uwnkvvihxkjaobxuazia.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3bmt2dmloeGtqYW9ieHVhemlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjMwMDMsImV4cCI6MjA2MDU5OTAwM30.Xr4R_iSRwItK7qSBPHRTq06YQMDT88pUcXuMu4ITcys';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
